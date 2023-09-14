07226e000eb17b208b044685017b76012bae4b97
sleepypower <daalmega@gmail.com>
2023-09-14 09:09:47 -0500

    Create utils.py

---
+++ lab5-dash/utils.py
@@ -0,0 +1,60 @@
+
+def get_country_coords(country_name, output_as=" boundingbox "):
+    """
+    Get the coordinates of a country by name .
+    : param : country_name : name of the country to retrieve coords
+    : param : output_as : "str to chose from "boundingbox " or "center ".
+    - "boundingbox " for [latmin , latmax , lonmin , lonmax ]
+    - "center " for [ latcenter , loncenter ]
+    """
+
+    # Create url
+    url = "{0}{1}{2} ". format("http :// nominatim.openstreetmap .org/ search ? country =",
+                               country_name, "& format = json & polygon =0 ")
+    response = requests.get(url).json()[0]
+
+    # parse response to list
+    if output_as == "boundingbox ":
+        lst = response[output_as]
+        output = [float(i) for i in lst]
+    if output_as == "center ":
+        lst = [response.get(key) for key in ["lon ", "lat "]]
+        output = [float(i) for i in lst]
+    return output
+
+
+def raw_processor(data_raw: pd . DataFrame):
+    data_raw["Training Start "] = pd . to_datetime(data_raw["Training Start "])
+    data_raw["Training Start_date "] = data_raw["Training Start "]. dt . date
+    data_raw . sort_values(by="Training Start_date ", ascending=True)
+
+    data = (data_raw . groupby([
+        "Country ",
+        "Vertical ",
+        "Language ",
+        "Status ",
+        "Training Start_date "])
+        [["Demand ",
+          "Offers Accepted ",
+          "Hired ",
+          "Applicants ",
+          "Requisition ID "]].agg({
+              "Demand ": "sum ",
+              "Offers Accepted ": "sum ",
+              "Hired ": "sum ",
+              "Applicants ": "sum ",
+              "Requisition ID ": "count "}) .
+        reset_index() . sort_values(by="Demand ", ascending=False))
+
+    countries_dict = {}
+    for country in data . Country . unique():
+        countries_dict[country] = get_country_coords(country, "center ")
+
+    data . rename(
+        columns={"Requisition ID ": " Requisition_count "}, inplace=True)
+    data["country_lat "] = data["Country "]. apply(lambda country_: countries_dict[
+        country_][1])
+    data["country_lon "] = data["Country "]. apply(lambda country_: countries_dict[
+        country_][0])
+
+    return data
